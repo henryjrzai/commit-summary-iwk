6b1fa01b15be94fdcdcafec03200ca9509f908df
Henry Junior Zai <94433467+henryjrzai@users.noreply.github.com>
2026-05-15 23:05:50 +0700

    ya

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
@@ -13,4 +13,3 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
